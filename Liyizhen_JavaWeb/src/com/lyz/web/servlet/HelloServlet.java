050115921d6e8925d915c1fde25f4b6a54cd4832
e <liyizhen@xlpayment.com>
2018-11-07 11:28:52 +0800

    commit all files in AllTests

---
+++ Liyizhen_JavaWeb/src/com/lyz/web/servlet/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.lyz.web.servlet;
+
+import java.io.IOException;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().write("hello servlet");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
---
+++ Liyizhen_JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Li_Yizhen
+  Date: 2018/11/6
+  Time: 16:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>人间词话</title>
+  </head>
+  <body>
+  渺万里层云，千山暮雪，只影向谁去
+  </body>
+</html>
